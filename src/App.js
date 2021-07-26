cd34e03f98a42889f806c9eb86772dec8ea6c8bd
Mario Delgado <mdelgado1138@hotmail.com>
2021-07-26 16:44:06 -0700

    setup clarifai 1

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Logo from './components/Logo/Logo';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Rank from './components/Rank/Rank';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 
 const particlesOptions = {
   particles: {
@@ -18,7 +20,38 @@ const particlesOptions = {
   },
 };
 
+const app = new Clarifai.App({
+  apiKey: '3fed091650b6444a98384f167efe87aa',
+});
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    };
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  };
+
+  onButtonSubmit = () => {
+    console.log('click');
+    app.models
+      .predict(
+        Clarifai.COLOR_MODEL,
+        // THE JPG
+        'https://i.insider.com/5d321d4ea209d3146d650b4a?width=1100&format=jpeg&auto=webp',
+      )
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   render() {
     return (
       <div className="App">
@@ -26,10 +59,11 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
-        {
-          // <FaceRecognition />
-        }
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        <FaceRecognition />
       </div>
     );
   }
---
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const FaceRecognition = () => {
+  return (
+    <div className="center">
+      <img
+        alt="face recog"
+        src="https://i.insider.com/5d321d4ea209d3146d650b4a?width=1100&format=jpeg&auto=webp"
+      ></img>
+    </div>
+  );
+};
+
+export default FaceRecognition;
