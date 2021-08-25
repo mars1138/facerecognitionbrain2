18b52f0dc36c83a233f0580d1161261c19a7cac4
LearnToCode1138 <mdelgado1138@hotmail.com>
2021-08-24 21:35:13 -0700

    validate input against blanks

--- src/App.js
+++ src/App.js
@@ -91,33 +91,39 @@ class App extends Component {
     console.log('click');
     this.setState({ imageUrl: this.state.input });
 
-    fetch('http://localhost:3000/imageurl', {
-      method: 'post',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        input: this.state.input,
-      }),
-    })
-      .then(response => response.json())
-      .then(response => {
-        console.log('imageurl response: ', response);
-        if (response) {
-          fetch('http://localhost:3000/image', {
-            method: 'put',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-              id: this.state.user.id,
-            }),
-          })
-            .then(res => res.json())
-            .then(count => {
-              this.setState(Object.assign(this.state.user, { entries: count }));
-            })
-            .catch(console.log);
-        }
-        this.displayFaceBox(this.calculateFaceLocation(response));
+    if (this.state.input === initialState.input) {
+      return console.log('No URL entered!');
+    } else {
+      fetch('http://localhost:3000/imageurl', {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          input: this.state.input,
+        }),
       })
-      .catch(err => console.log(err));
+        .then(response => response.json())
+        .then(response => {
+          console.log('imageurl response: ', response);
+          if (response) {
+            fetch('http://localhost:3000/image', {
+              method: 'put',
+              headers: { 'Content-Type': 'application/json' },
+              body: JSON.stringify({
+                id: this.state.user.id,
+              }),
+            })
+              .then(res => res.json())
+              .then(count => {
+                this.setState(
+                  Object.assign(this.state.user, { entries: count })
+                );
+              })
+              .catch(console.log);
+          }
+          this.displayFaceBox(this.calculateFaceLocation(response));
+        })
+        .catch(err => console.log(err));
+    }
   };
 
   onRouteChange = route => {
