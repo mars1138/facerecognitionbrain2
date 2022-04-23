adaf7fbf441d715673620f21f23266969a21e161
LearnToCode1138 <mdelgado1138@hotmail.com>
2022-04-22 19:24:47 -0700

    update react

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Rank = ({ name, entries }) => {
   return (
-    <div>
+    <div className="mt4">
       <div className="white f3">{`${name}, your current entry count...`}</div>
       <div className="white f1">{`#${entries}`}</div>
     </div>
